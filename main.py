b7f88243a49784fa11d6e33703e1eb87ba009e97
Abdullah Furkan Genç <2twofurkan@gmail.com>
2025-07-09 15:45:00 +0300

    updated requirement.txt

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 API_URL = os.getenv("API_URL")
-print(API_URL)
 API_TOKEN = os.getenv("API_TOKEN")
 transport = RequestsHTTPTransport(
     url=API_URL,
